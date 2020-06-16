5b429750a28a0a86f967c17803c53f353ac2d99c
Carolina <carolti2013@gmail.com>
2020-06-16 15:51:21 -0300

    rotas de autenticaçao

--- src/routes/medico-route.js
+++ src/routes/medico-route.js
@@ -4,9 +4,15 @@ const express = require('express');
 const router = express.Router();
 
 const controller = require('../controllers/medico-controller')
+const authService = require('../services/auth-service');
 
 router.post("/", controller.post);
 router.put("/:id",controller.put);
 router.delete("/", controller.delete);
+
+router.post('/authenticate', controller.authenticate);
+router.post('/refresh-token', authService.authorize, controller.refreshToken);
+
+
 
 module.exports = router;  
